191d8197436a27dc26f29b3842ebc77f251c8ef2
gksthf4545 <104492925+gksthf4545@users.noreply.github.com>
2022-05-19 01:04:00 +0900

    Add files via upload

---
+++ DialogBaseDlg.cpp
@@ -0,0 +1,214 @@
+﻿
+// DialogBaseDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "DialogBase.h"
+#include "DialogBaseDlg.h"
+#include "afxdialogex.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+using namespace std;
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDialogBaseDlg 대화 상자
+
+
+
+CDialogBaseDlg::CDialogBaseDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+
+	string ReadFull = "";
+
+	fstream ReadFile;
+	ReadFile.open("D:\\한기대\\3학년\\응용프로그래밍\\termproject\\월인천강지곡 권상\\typeDB.csv", ios::in );
+	bool FileOpen = ReadFile.is_open();
+	if (FileOpen)
+	{
+		MessageBox(_T("file을 열었습니다.\n"));
+		//파일의 문자열 읽기
+		char chReadLine[4096];
+		string ReadLine = "";
+		int i;
+		while (ReadFile.getline(chReadLine, 4096))
+		{
+			ReadLine = chReadLine;
+			vector<string> result = split(str, ',');
+			for (int i = 0; i < result.size(); i++)
+			{
+				cout << result[i] << "\n";
+			}
+			ReadFull += ReadLine;
+
+		}
+
+		MessageBox(_T("읽은 파일의 데이터는\n"));
+		cout <<  ReadLine;
+
+
+	}
+	else
+	{
+		MessageBox(_T("file을 열지 못했습니다..\n"));
+	}
+
+	ReadFile.close();
+	//23:00
+		/*
+		readFile.open("typeDB.txt");
+		if (readFile.is_open())
+		{
+			while (!readFile.eof())
+			{
+				char arr[256];
+				readFile.getline(arr, 256);
+				for (int i = 0; i < 255; i++)
+				{
+
+				}
+			}
+		}
+		readFile.close();*/
+	
+}
+
+void CDialogBaseDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+
+}
+
+BEGIN_MESSAGE_MAP(CDialogBaseDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CDialogBaseDlg 메시지 처리기
+
+BOOL CDialogBaseDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CDialogBaseDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CDialogBaseDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CDialogBaseDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ DialogBaseDlg.h
@@ -0,0 +1,56 @@
+﻿
+// DialogBaseDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CDialogBaseDlg 대화 상자
+class CDialogBaseDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CDialogBaseDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+};
+
+//SCharInfo 구조체
+struct SCharInfo {
+	CString m_char;
+	int m_type;
+	int m_sheet;
+	int m_sx;
+	int m_sy;
+	int m_line;
+	int m_order;
+	int m_width;
+	int m_height;
+};
+
+//CTypeDB클래스
+class CTypeDB {
+	int m_nSheet;
+	int m_nChar;
+	int m_Chars;
+	BOOL readcsvfile;
+};
